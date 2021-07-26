fcce1f889e22c93b8c9becde8e7b13742d5504c8
vishnuprasad1992 <75542863+vishnuprasad1992@users.noreply.github.com>
2021-07-26 10:25:16 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -52,10 +52,6 @@ const Navbar = () => {
                                 <Link className="nav-link wheat" to="#!" tabIndex="-1" aria-disabled="true">Contact us</Link>
                             </li>
                         </ul>
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </nav>
